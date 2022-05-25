0c5cf68d3bc6365ca9d688e625eb77cf370cf1e0
farnatasha <2020898386@student.uitm.edu.my>
2022-05-25 15:22:43 +0800

    3

--- next_page.php
+++ next_page.php
@@ -4,13 +4,15 @@ session_start();
 echo "<html>";
 
 $sid = session_id();
-echo "Session ID returned by session_id(): ".$sid."\n";
+echo "Session ID returned by session_id(): ".$sid."\n"; 
+echo "<br>";
 $sid = SID;
 echo "Session ID returned by SID: ".$sid."\n";
+echo "<br>";
 
 $myLogin = $_SESSION["myLogin"];
 echo "Value of myLogin has been retrieved: ". $myLogin."\n";
-
+echo "<br>";
 $myColor = $_SESSION["myColor"];
 echo "Value of myLogin has been retrieved: ". $myColor."\n";
 

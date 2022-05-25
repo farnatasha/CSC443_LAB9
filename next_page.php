4c294ab2af8c3641ff54fe86dfb91fd43443feb4
farnatasha <2020898386@student.uitm.edu.my>
2022-05-25 15:35:12 +0800

    3

--- next_page.php
+++ next_page.php
@@ -3,12 +3,23 @@
 session_start();
 echo "<html>";
 
-$sid = session_id();
+/* $sid = session_id();
 echo "Session ID returned by session_id(): ".$sid."\n"; 
 echo "<br>";
 $sid = SID;
 echo "Session ID returned by SID: ".$sid."\n";
+echo "<br>";*/
+
+$queryString = $_SERVER["QUERY_STRING"];
+echo "Query string of the incoming URL: ".$queryString."\n";
 echo "<br>";
+
+echo "Cookies received: \n";
+foreach ($_COOKIE as $name => $value) {
+    echo "<br>";
+    echo " $name = $value\n";
+}
+
 
 $myLogin = $_SESSION["myLogin"];
 echo "Value of myLogin has been retrieved: ". $myLogin."\n";

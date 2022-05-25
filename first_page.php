c15fbcdab76b7372be91aca2343a5f7962f55fca
farnatasha <2020898386@student.uitm.edu.my>
2022-05-25 15:09:40 +0800

    1

---
+++ first_page.php
@@ -0,0 +1,16 @@
+<?php
+session_start();
+echo "<html>";
+
+$_SESSION["myLogin"] =  "myWebsite";
+echo "A value saved in the session named as myLogin.<br>";
+
+$_SESSION["myColor"] =  "Light Blue";
+echo "A value saved in the session named as myColor.<br>";
+
+echo "Click <a href=next_page.php>Next Page</a>" 
+      . "to retrieve the values.<br>";
+
+      echo "</html><br>";
+
+      ?>

ed80953ece48c1a1ce78d1c400c19a960c4ffd88
farnatasha <2020898386@student.uitm.edu.my>
2022-05-25 16:12:44 +0800

    2

--- setting_persistent_cookies.php
+++ setting_persistent_cookies.php
@@ -6,21 +6,25 @@ setcookie ("CouponValue", "100.00",time()+60*60*24*7);
 
 print("\n");
 print("2 temporary cookies were delivered.\n");
+echo "<br>";
 print("2 consistent cookies were delivered.\n");
+echo "<br>";
 
 if (isset($_COOKIE["myLoginName"])) {
     $loginName = $_COOKIE["myLoginName"];
     print ("Received a cookie named as myLoginName: ".$loginName."\n");
+    echo "<br>";
 } else {
     print ("Did not received a cookie named as myLoginName.\n");
 }
-
+echo "<br>";
 $count = count ($_COOKIE);
 print ("$count cookies received.\n");
 foreach ($_COOKIE as $name => $value) {
+    echo "<br>";
     print " $name = $value\n";
 }
-
+echo "<br>";
 print("\n");
 
 ?>

42f93fe1c71b1d803f8fa95509812d261399e64b
farnatasha <2020898386@student.uitm.edu.my>
2022-05-25 16:20:49 +0800

    3

--- removing_cookies.php
+++ removing_cookies.php
@@ -4,7 +4,7 @@ setcookie ("CouponValue",time()-1);
 print ("<pre>\n");
 echo "<br>";
 print ("2 cookies were delivered with past times.\n");
-echo "<br>" 
+echo "<br>"; 
 
 $count = count ($_COOKIE);
 print("$count cookies received.\n");
